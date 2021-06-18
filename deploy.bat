13e6f6c29d6a87ce89a34dda544609565f4d4032
ineedbots <ineedbots@outlook.com>
2021-06-18 16:12:07 -0600

    Add astyle and deploy

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+xcopy cod2m_bot_warfare\mods\bots\maps mods\bots\maps\ /Y /I /E /H /C
+xcopy cod2m_bot_warfare\mods\bots\scriptdata mods\bots\scriptdata\ /Y /I /E /H /C
+xcopy cod2m_bot_warfare\mods\bots\bots.txt mods\bots\ /Y /I /H /C
