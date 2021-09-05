a91bde05aa9061aba6a06244c1808e8cf6715496
ineedbots <ineedbots@outlook.com>
2021-09-05 15:03:44 -0600

    CoD2 server stuff

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
 xcopy cod2m_bot_warfare\mods\bots\maps mods\bots\maps\ /Y /I /E /H /C
 xcopy cod2m_bot_warfare\mods\bots\scriptdata mods\bots\scriptdata\ /Y /I /E /H /C
+xcopy cod2m_bot_warfare\main main\ /Y /I /E /H /C
 xcopy cod2m_bot_warfare\mods\bots\bots.txt mods\bots\ /Y /I /H /C
